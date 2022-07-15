91666978766859fabee6ddc7e2ed8ad857378720
rootgit23 <rmsgh1009@naver.com>
2022-07-15 15:51:46 +0900

    main

--- src/com/im/main/Main.java
+++ src/com/im/main/Main.java
@@ -1,12 +1,17 @@
 package com.im.main;
 
 import com.im.terran.Marine;
+import com.im.terran.Scv;
+import com.im.unit.Unit;
 import com.im.zerg.Hydra;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Marine m1 = new Marine();
+		Unit unit = m1;
+		m1 = (Marine)unit;
+		Scv s1 = new Scv();
 		m1.setHp(30);
 		Hydra h1 = new Hydra();
 		h1.attack();
