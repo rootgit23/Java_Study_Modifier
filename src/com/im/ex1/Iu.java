79f70c2a8a519713a7f7b5c60347f737f070392c
rootgit23 <rmsgh1009@naver.com>
2022-07-15 10:15:28 +0900

    singleton

---
+++ src/com/im/ex1/Iu.java
@@ -0,0 +1,36 @@
+package com.im.ex1;
+
+public class Iu {
+	
+	private String name;
+	private int age;
+	
+	private static Iu iu=null;
+	
+	private Iu() {
+		
+	}
+	
+	public static Iu setIu() {
+		if(Iu.iu == null) {
+			Iu.iu = new Iu();
+		}
+		return Iu.iu;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
